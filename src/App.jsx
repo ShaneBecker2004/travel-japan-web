9cfe136019a8f5d58461366bf1738148cefaa856
ShaneBecker2004 <shanemkbecker.2004@gmail.com>
2026-03-12 20:07:21 -0700

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,75 @@
-import './App.css';
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+function Home() {
+  return (
+    <div className="content">
+      <img src="/images/Nippon_Navigator_Logo.png" alt="Nippon Navigator" />
+      <h3>Welcome to Nippon Navigator!</h3>
+    </div>
+  )
+}
+
+function Explore() {
+  return (
+    <div className="content">
+      <h3>Explore Japan</h3>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div>
-        <nav className="navbar">
-            <a href="index.html" className="logo">
-                <img src="/images/Flag_of_Japan.svg.png" alt="Nippon Navigator" />
-            </a>
+    <BrowserRouter>
+
+      <nav className="navbar">
+        <Link to="/" className="logo">
+          <img src="/images/Flag_of_Japan.svg.png" alt="Nippon Navigator" />
+        </Link>
 
         <div className="nav-links">
-                <a className="active" href="index.html">Home</a>
-            <div className="dropdown">
-                <button className="dropbtn">About
-                <i className="fa fa-caret-down"></i>
-                </button>
+
+          <Link to="/">Home</Link>
+
+          <div className="dropdown">
+            <button className="dropbtn">About</button>
             <div className="dropdown-content">
-                <a href="aboutus.html">About Us</a>
-                <a href="commonphrase.html">Common Phrases</a>
+              <Link to="/about">About Us</Link>
+              <Link to="/phrases">Common Phrases</Link>
             </div>
-            </div>
-                <a href="explore.html">Explore</a>
-            <div className="dropdown">
-                <button className="dropbtn">Others
-                <i className="fa fa-caret-down"></i>
-                </button>
+          </div>
+
+          <Link to="/explore">Explore</Link>
+
+          <div className="dropdown">
+            <button className="dropbtn">Others</button>
             <div className="dropdown-content">
-                <a href="map.html">Map</a>
-                <a href="exchangerate.html">Exchange Rates</a>
-                <a href="japan-immigration.html">Japan Immigration</a>
+              <Link to="/map">Map</Link>
+              <Link to="/exchange">Exchange Rates</Link>
+              <Link to="/immigration">Japan Immigration</Link>
             </div>
-            </div>
-                <a href="index.html">Create Plan</a>
+          </div>
 
-                <a className="account" href="index.html">Account</a>
+          <Link to="/plan">Create Plan</Link>
+
+          <Link to="/account" className="account">Account</Link>
+
         </div>
-        </nav>
+      </nav>
 
-        <div className="content">
-            <img src="/images/Nippon_Navigator_Logo.png" alt="Nippon Navigator" />
-            <h3>Welcome to Nippon Navigator!</h3>
-        </div>
 
-        <footer className="footer">
-            Nippon Navigator @2026 Test
-        </footer>
-      </div>
-  );
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+      </Routes>
+
+
+      <footer className="footer">
+        Nippon Navigator @2026 Test
+      </footer>
+
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
